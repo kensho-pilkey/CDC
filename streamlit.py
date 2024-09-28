57639bc0921585fb5ece646816cffc87e9b6b6ae
kensho-pilkey <kpilkey@unc.edu>
2024-09-28 14:07:30 -0400

    streamlit

---
+++ streamlit.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+@st.cache
+def load_data():
+    df = pd.read_excel('data/Health_Science_Dataset.xlsx')
+    return df
+
+st.title("COVID-19, Pneumonia, and Influenza Mortality Dashboard")
+st.write("This application visualizes mortality trends for COVID-19, pneumonia, and influenza using historical data.")
+
+df = load_data()
+
+if st.checkbox("Show raw data"):
+    st.write(df.head())
